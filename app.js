0cf28cd7de2f3c767de126612fd1dc0df3b01761
Fakile Temitope Oluwaseyi <fakiletemitope@gmail.com>
2025-05-09 21:08:11 +0100

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { user_router } from "./src/route/user-router.js"
 import bodyParser from "body-parser"
 import { syncDb } from "./src/database/db_init.js"
 import { auth_router } from "./src/route/auth-router.js"
+import cors from 'cors'
 const app = express()
 
 const port = config.port || 3000
@@ -11,6 +12,7 @@ const port = config.port || 3000
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 //routes
 app.use("/auth", auth_router)
